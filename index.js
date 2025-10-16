b04d6755b8f1b82992601c8597e0911f189a5efa
EvaSunCatches <eva.suncatches@gmail.com>
2025-10-16 22:58:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './src/app/app.component.ts'; // Используем локальный импорт
 
-// Запуск (бутстраппинг) приложения с помощью импортированного AppComponent
-bootstrapApplication(AppComponent)
-  .catch((err) => console.error(err));
+// Динамический импорт, чтобы избежать циклической зависимости (Uncaught ReferenceError)
+// Этот метод гарантирует, что AppComponent будет доступен после инициализации Angular.
+import('./src/app/app.component.ts')
+  .then(module => {
+    // В случае динамического импорта, компонент находится в .AppComponent
+    bootstrapApplication(module.AppComponent)
+      .catch((err) => console.error('Ошибка бутстраппинга:', err));
+  })
+  .catch((err) => console.error('Ошибка загрузки компонента:', err));
