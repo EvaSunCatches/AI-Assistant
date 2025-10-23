93caefa1ba74711f163d9e8d90b9d4b7d6be3579
EvaSunCatches <eva.suncatches@gmail.com>
2025-10-23 18:20:15 +0300

    Update api_config.js

--- assets/api_config.js
+++ assets/api_config.js
@@ -1,7 +1,3 @@
 // assets/api_config.js
-
-// ВСТАВТЕ ВАШ НОВИЙ, НІДЕ НЕ ОПУБЛІКОВАНИЙ, ДІЙСНИЙ КЛЮЧ GEMINI API У ПОДВІЙНИХ ЛАПКАХ
 const GEMINI_API_KEY = "AIzaSyCy0oVEmVk4vp3c5BiVonQeDwTJdWuVLBs"; 
-
-// Робимо ключ доступним глобально для AI-Assistant.js
 window.GEMINI_API_KEY = GEMINI_API_KEY;
